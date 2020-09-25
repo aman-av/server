10882f65ff0f9508f0209806ae2f2e7fc198236e
Aman Verma <itz.aman.av@gmail.com>
2020-09-25 18:45:54 +0530

    finished google auth

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+    googleClientID: '1051823412802-73606jl27sj4au1dis7v0j42njjd17v7.apps.googleusercontent.com',
+    googleClientSecret: 'a3d5PTcMdi6aGIzrHRNVDNbl',
+    mongoURI: 'mongodb+srv://aman:zxcv1729@emaily-dev.dcxkv.mongodb.net/server?retryWrites=true&w=majority',
+    cookieKey: 'bdjhbiuxkjbdtvuujrneljzaniuhvndsjnfiuwekdjhhbcliu'
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+const express =  require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+const app = express();
+
+app.use(
+    cookieSession({
+
+        maxAge : 30 * 24 * 60 * 60 * 1000,
+        keys : [keys.cookieKey]
+    }
+    )
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+
+const PORT= process.env.PORT || 8080;
+app.listen(PORT, function() {
+    console.log('Our app is running on http://localhost:' + PORT);
+});
+
+//http://localhost:5000/
